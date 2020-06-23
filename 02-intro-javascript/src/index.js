102155df9e5bc28b5fbcaa0475f24a0322184ad8
Alejandro E. Rendon <alejandro@rendon.co>
2020-06-22 21:03:00 -0500

    3-13 Const and let

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,16 @@
+// Variables and constants
+const name = 'Alejandro';
+const lastName = 'Rendon';
+
+let someValue = 6;
+someValue = 4
+
+console.log(name, lastName, someValue);
+
+if (true) {
+    const name = 'Peter'
+
+    console.log(name);
+}
+
+console.log(someValue);
